39417399eab8e25d3ca1ff36e77847b4acec4842
Dathless <dangphamfamily2004@gmail.com>
2026-07-06 20:08:35 +0700

    chore:update startup script

--- run_project.bat
+++ run_project.bat
@@ -1,5 +1,7 @@
 @echo off
 
+call init_project.bat
+
 start "Backend" cmd /k "cd ./lms_api && ..\venv\Scripts\python.exe manage.py runserver"
 
 start "Frontend" cmd /k "cd ./lms_frontend && npm run dev"
