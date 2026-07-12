1e11294f16316b83ffb417d98ce157baf04c3d67
chiencaycuoc <vonhuchien2004@gmail.com>
2026-07-12 17:10:49 +0700

    update analytics

--- lms_api/analytics/apps.py
+++ lms_api/analytics/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class AnalyticsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'analytics'
+    """Cấu hình ứng dụng thống kê."""
 
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "analytics"
